fd1b9fe3628b721f3f0c7656403025b9ebb6b4bc
Sakhavat1996 <sakhavat1996@gmail.com>
2024-03-09 22:15:42 +0400

    re-commit

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,10 +1,13 @@
-export default function Products() {
-    const products = 'https://dummyjson.com/docs/products'
-    fetch(products)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+import { useEffect } from "react"
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err=>console.log(err))
+    },[])
     return(
         <>
             <h1>smth</h1>
